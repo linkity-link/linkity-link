d95c105494a9cce42e2cc2d60851204cd6d7ac99
madprogramer <themadprogramer@gmail.com>
2020-12-11 00:39:21 +0300

    Added UserData

--- linkitylink/public/demos/LDP/Simulation copy.js
+++ linkitylink/public/demos/LDP/Simulation copy.js
@@ -1,14 +1,11 @@
 //Linear Differntial Privacy Demo!
 
 //IMPORTS
-import {UserData} from './UserData.js';
-
 
 
 //Run when demo loads:
 document.addEventListener("DOMContentLoaded", event=>{
 
 	console.log("Loaded page!");
-	UserData.test()
 
 });
---
+++ linkitylink/public/demos/LDP/UserData.js
@@ -0,0 +1,8 @@
+function UserData(id) {
+  this.id = 0;
+  this.test = function(){
+  	console.log("TEST")
+  }
+};
+
+export{ UserData };
