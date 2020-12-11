b5300a2fdbc58553322c68f729d48d394861a3d9
madprogramer <themadprogramer@gmail.com>
2020-12-12 00:32:41 +0300

    Updated UserData, UI to allow .csv uploads
    WIP: get Data

--- linkitylink/public/demos/LDP/LDP.js
+++ linkitylink/public/demos/LDP/LDP.js
@@ -1,7 +1,7 @@
 //Linear Differntial Privacy Demo!
 
 //IMPORTS
-import {UserData} from './UserData.js';
+import {UserData, UserDataReader} from './UserData.js';
 import {Privatizer} from './Privatizer.js';
 import {Histogram} from './Histogram.js';
 import {Simulator} from './Simulator.js';
@@ -11,5 +11,12 @@ import {Simulator} from './Simulator.js';
 document.addEventListener("DOMContentLoaded", event=>{
 
 	console.log("Loaded page!");
+	//console.log(UserDataReader(1000))
 
 });
+
+//When table uploaded:
+
+document.getElementById('fileupload').onchange = function(){
+   UserDataReader(this,10);
+}
--- linkitylink/public/demos/LDP/UserData.js
+++ linkitylink/public/demos/LDP/UserData.js
@@ -3,4 +3,47 @@ function UserData(id,age) {
     this.age = age;
 };
 
-export{ UserData };
+ 
+function UserDataReader(input, maxsize) {
+    //console.log(input)
+    console.log("loading file...")
+ if (input.files && input.files[0]) {
+    let reader = new FileReader();
+    reader.readAsBinaryString(input.files[0]);
+    reader.onload = function (e) {
+        //console.log(e.target.result, maxsize)
+        getData(e.target.result, maxsize)           
+    }  
+    }
+}
+ 
+function getData(data, maxsize){
+    console.log("Ignoring maxsize for now")
+    let tableData = [];
+    let lbreak = data.split("\n");
+    //console.log(lbreak.length)
+    lbreak.forEach(res => {
+        tableData.push(res.split(","));
+    });
+    
+    //tableData.push(res.split(","));
+    //console.table(tableData);
+    //console.log(tableData.length)
+
+    cut = Math.max(maxsize, tableData.length)
+
+    tableData = tableData.subarray(1,cut)
+
+    TODO: FIND INDEX OF "AGE" IN THE FIRST ROW
+
+    TODO: Make UserData ON EVERY ["AGE"] in tableData, and collect into an Array
+
+    return Array
+
+    //REST OF THE CODE GOES IN Simulation.js
+    //Privatize Array
+    //Send to Histogram
+    //Compare results?
+}
+
+export{ UserData, UserDataReader };
