c149173a39b7fbc63472bf174add32725d1c292d
cansudoganay <40060879+cansudoganay@users.noreply.github.com>
2020-12-11 17:00:50 +0300

    Update Privatizer.js

--- linkitylink/public/demos/LDP/Privatizer.js
+++ linkitylink/public/demos/LDP/Privatizer.js
@@ -1,7 +1,23 @@
 
 function Privatizer(obj) {
 	//Privatize!
+	var v = randomInt(1, 100)
+	var p = randomInt(0, 100)
+	
+	if(p<=5){
+		return obj
+	}else{
+		obj = v
+		return obj
+	}
+	
 	return obj
 }
+
+function randomInt(low, high) {
+  return Math.floor(Math.random() * (high - low) + low)
+}
+
+
 
 export{ Privatizer };
