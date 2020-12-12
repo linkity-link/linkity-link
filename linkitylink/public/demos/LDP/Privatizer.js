89db441e7dd968534bfedd55d458d13f4df2e506
madprogramer <themadprogramer@gmail.com>
2020-12-12 22:17:00 +0300

    Syntax improvements

--- linkitylink/public/demos/LDP/Privatizer.js
+++ linkitylink/public/demos/LDP/Privatizer.js
@@ -1,21 +1,21 @@
 
 function Privatize(obj) {
 	//Privatize!
-	var v = randomInt(10, 101) //minimum age of our user data is 10, maximum age is 100
-	var p = randomInt(0, 101)  //probability
+	var v = randomInt(10, 101); //minimum age of our user data is 10, maximum age is 100
+	var p = randomInt(0, 101);  //probability
 	
 	if(p<=5){ //with probability less then or equal to %5 we say the truth
-		return obj
+		return obj;
 	}else{ //we transform the truth to a random value 
-		obj = v
-		return obj
+		obj = v;
+		return obj;
 	}
 	
-	return obj
+	return obj;
 }
 
 function randomInt(low, high) {
-  return Math.floor(Math.random() * (high - low) + low)
+  return Math.floor(Math.random() * (high - low) + low);
 }
 
 export{ Privatize, randomInt };
